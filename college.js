76e8e6fd7248702468e682f33b0f488b43c927ca
Abhyudaya2005 <tiwarishikhar608@gmail.com>
2025-09-26 23:33:30 +0530

    yes

---
+++ college.js
@@ -0,0 +1,12 @@
+// // // event parameter - parameter passed to a function which lisens to an event //
+// // document.getElementById('text-input').addEventListener('focus'.at(e) => {
+// //     console.log"button was clickeed
+// //     console.log"button was clickeed
+// // }
+//  const myTextBox = document.getElementById('text-input')
+//  document.getElementById('submitBtn').addEventListener('click',(e) => (
+//     e.preventDefault()
+//  ) 
+
+// create html element using dom
+console.log( document.createElement('div'));
---
+++ script.js
@@ -0,0 +1,1 @@
+alert("welcome to abhyudaya tewari page")
